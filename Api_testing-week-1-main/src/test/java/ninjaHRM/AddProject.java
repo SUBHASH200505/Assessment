3c0d1bc59ccf31a759c872a8646e2c855e72e7a2
SUBHASH200505 <163117768+SUBHASH200505@users.noreply.github.com>
2026-04-09 09:27:55 +0530

    Update AddProject.java

--- Api_testing-week-1-main/src/test/java/ninjaHRM/AddProject.java
+++ Api_testing-week-1-main/src/test/java/ninjaHRM/AddProject.java
@@ -10,10 +10,10 @@ public class AddProject {
 		.contentType("application/xml")
         .body("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n"
         		+ "<Project>\r\n"
-        		+ "	<createdBy>MoitheGreat</createdBy>\r\n"
-        		+ "	<projectName>test321</projectName>\r\n"
+        		+ "	<createdBy>rockstar</createdBy>\r\n"
+        		+ "	<projectName>admin</projectName>\r\n"
         		+ "	<status>Completed</status>\r\n"
-        		+ "	<teamSize>0</teamSize>\r\n"
+        		+ "	<teamSize>1</teamSize>\r\n"
         		+ "</Project>")
         
 		// HTTP METHOD
