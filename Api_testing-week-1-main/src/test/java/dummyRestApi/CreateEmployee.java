3e8d7f79ad5483d6acbc02e56fa3552797b9b854
SUBHASH200505 <163117768+SUBHASH200505@users.noreply.github.com>
2026-04-09 09:15:25 +0530

    Update employee creation data in CreateEmployee.java

--- Api_testing-week-1-main/src/test/java/dummyRestApi/CreateEmployee.java
+++ Api_testing-week-1-main/src/test/java/dummyRestApi/CreateEmployee.java
@@ -7,7 +7,7 @@ public class CreateEmployee {
 	public static void main(String[] args) {
 		RestAssured.given()
 		.contentType("application/json")
-		.body("{\"name\":\"test\",\"salary\":\"123\",\"age\":\"23\"}")
+		.body("{\"name\":\"admin\",\"salary\":\"50000\",\"age\":\"11\"}")
 		
 		.when()
 			.post("https://dummy.restapiexample.com/api/v1/create")
